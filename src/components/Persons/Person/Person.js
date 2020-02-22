64774aee78309c1b43438d40cec0c90238dcb325
Max Dayton <daytonmaximus@gmail.com>
2020-02-22 09:53:43 -0700

    lecture 111

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-// import Aux from '../../../hoc/Auxilliary';
+import PropTypes from 'prop-types';
+import Aux from '../../../hoc/Auxilliary';
+import withClass from '../../../hoc/withClass';
 import classes from './Person.css'
-import WithClass from '../../../hoc/WithClass';
 
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render () {
         console.log('[Person.js] rendering...');
         return(
-            <WithClass classes={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <Aux>
+                <p key="i2" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} yearss old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </WithClass>
+                <input 
+                    key="i3" 
+                    // ref={(inputEl) => {this.inputElement = inputEl}} 
+                    ref={this.inputElementRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                />
+            </Aux>
         );
     }
 };
 
-export default Person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxilliary';
 
 
 class App extends Component {
@@ -19,7 +20,8 @@ class App extends Component {
       { id: 'sdfsy65', name: 'Java', age: 1 },
     ],
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state){
@@ -69,7 +71,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( {persons: persons} );
+    this.setState((prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1
+      }
+    });
   }
 
   deletePersonHandler = (personIndex) => {
@@ -98,7 +105,7 @@ class App extends Component {
     }
   
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={ () => { this.setState({ showCockpit: false })}}>Remove Cockpit</button>
         {this.state.showCockpit ? (
           <Cockpit 
@@ -109,9 +116,9 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
